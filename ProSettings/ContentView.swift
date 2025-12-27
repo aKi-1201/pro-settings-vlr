c9922c2795746b9461a454920465f5eece489b46
aKi-1201 <b10501015@ntu.edu.tw>
2025-12-27 18:08:43 +0800

    Initial Commit

---
+++ ProSettings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProSettings
+//
+//  Created by 114-1iosClassStudent05 on 2025/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProSettings/ProSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProSettingsApp.swift
+//  ProSettings
+//
+//  Created by 114-1iosClassStudent05 on 2025/12/27.
+//
+
+import SwiftUI
+
+@main
+struct ProSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
